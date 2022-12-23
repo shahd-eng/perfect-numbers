9edefd541b1cda7ef3888e722ae10981b8bf3c81
shahd-eng <114587741+shahd-eng@users.noreply.github.com>
2022-12-23 21:51:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter The Begning : ");
+
+ int firstnum = int.Parse(Console.ReadLine());
+
+Console.WriteLine("please enter the End: ");
+
+int secondnum = int.Parse(Console.ReadLine());
+
+Console.WriteLine("The Perfect numbers within the given range : ");
+
+for (int j = firstnum; j <= secondnum; j++)
+{
+   int i = 1;
+   int sum = 0;
+    while (i < j)
+    {
+        if (j % i == 0)
+            sum = sum + i;
+        i++;
+    }
+    if (sum == j)
+        Console.WriteLine("{0} ", j);
+}
+Console.WriteLine("  ");
